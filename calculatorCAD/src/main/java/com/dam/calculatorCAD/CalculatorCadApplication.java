7b1c7869da35f3246a520ebef51667229878c143
Daniel Cuervo Álvarez <daniel.cuervo@informatica.cifpaviles.pa>
2026-04-23 20:15:06 +0200

    subido proyecto

---
+++ calculatorCAD/src/main/java/com/dam/calculatorCAD/CalculatorCadApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorCAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorCadApplication.class, args);
+	}
+
+}
---
+++ calculatorCAD/src/test/java/com/dam/calculatorCAD/CalculatorCadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorCAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorCadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
